ceec73d338b48c39c65088ef065a27bbd25aa660
Bilal Osmany <mohammedbilalosmany@gmail.com>
2024-11-30 12:57:43 +0530

    Update vpc.tf

--- network/vpc.tf
+++ network/vpc.tf
@@ -1,16 +1,17 @@
-resource "aws_vpc" "default" {
+resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
   enable_dns_hostnames = true
+
   tags = {
-    Name        = "${var.vpc_name}"
-    Owner       = "Raj"
-    environment = "${var.environment}"
+    Name = "${var.environment}-vpc"
   }
 }
 
-resource "aws_internet_gateway" "default" {
-  vpc_id = aws_vpc.default.id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
   tags = {
-    Name = "${var.vpc_name}-IGW"
+    Name = "${var.environment}-igw"
   }
 }
