8294572e44fea23f3329b0b6f3cc7196d32fdabc
Bilal Osmany <mohammedbilalosmany@gmail.com>
2025-03-18 10:56:39 +0530

    Update outputs.tf

--- Development/outputs.tf
+++ Development/outputs.tf
@@ -1,3 +1,35 @@
-output "natgwid" {
+output "compute_instance_id" {
+  value = module.dev_compute_1.instance_id
+}
+
+output "elb_dns_name" {
+  value = module.dev_elb_1.elb_dns_name
+}
+
+output "elb_public_dns_name" {
+  value = module.dev_elb_1_public.elb_dns_name
+}
+
+output "iam_instance_profile" {
+  value = module.dev_iam_1.instprofile
+}
+
+output "vpc_id" {
+  value = module.dev_vpc_1.vpc_id
+}
+
+output "public_subnets_id" {
+  value = module.dev_vpc_1.public_subnets_id
+}
+
+output "private_subnets_id" {
+  value = module.dev_vpc_1.private_subnets_id
+}
+
+output "nat_gateway_id" {
   value = module.dev_natgw_1.natgw_id
+}
+
+output "security_group_id" {
+  value = module.dev_sg_1.sg_id
 }
