0e3cef9272092a84d5b8a4e279868a73a5017e81
Bilal Osmany <mohammedbilalosmany@gmail.com>
2025-03-18 10:56:56 +0530

    Update sg.tf

--- Development/sg.tf
+++ Development/sg.tf
@@ -1,5 +1,5 @@
 module "dev_sg_1" {
-  source        = "../modules/sg"
+  source        = "../Modules/sg"
   vpc_name      = module.dev_vpc_1.vpc_name
   vpc_id        = module.dev_vpc_1.vpc_id
   environment   = module.dev_vpc_1.environment
